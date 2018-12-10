c3f52a2f72ec879b29a8802540898245d951c0c0
grim117reaper <amanscgo@gmail.com>
2018-12-10 06:57:42 +0530

    reverted

--- Scraping-Meddra/README.md
+++
@@ -1,2 +0,0 @@
-# Scraping-Meddra
-need to remove l0 l1 and l2 tags from output files in split folder
--- Scraping-Meddra/split/xml_parser.py
+++
@@ -1,4 +0,0 @@
-import requests
-import xml.etree.ElementTree as ET
-tree = ET.parse("combined.xml")
-root = tree.getroot()
